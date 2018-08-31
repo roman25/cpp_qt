9c8efc518291f30b262074728b8ff323e6d162c1
roman25 <ro25man@yandex.ru>
2018-08-31 23:04:25 +0300

    Update to collect statistic about pins

--- results_processing/main.cpp
+++ results_processing/main.cpp
@@ -1,33 +1,64 @@
 #include <iostream>
-#include <string>
 #include <fstream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-// Set test cases
-const string openCase = "Open";
-const string shortCase = "Short";
-const string okCase = "Ok";
+// Set required test cases
+const string openCase   = "Open";
+const string shortCase  = "Short";
 
+// Set parameters to parse and store data
+const string fileName               = "_cases_statistic";
+const string outputFileExtension    = ".txt";
+const string inputFileExtension     = ".txt";
+string delimiter                    = "\t";
+size_t delimeterPosition            = 0;
 
-// Function for reading input data
-void readInputData(string directoryPath, int *counterOpen, int *counterShort, int *counterOk)
+void collectPinNames (string directoryPath, vector <string> *pins)
 {
-    // Define input file extension
-    const string inputFileExtension = ".txt";
+    /* Function collects name of contacts an integrated circuit */
 
-    // Define variables that are help form full file name
+    // Set full path to a file to collect contact names
+    string filePath = directoryPath + "1" + inputFileExtension;
+
+    // Create input stream
+    string inputData;
+    ifstream readData(filePath);
+
+    string pinName;
+
+    // Iterate for every row in the stream
+    while(getline(readData, inputData))
+    {
+        delimeterPosition = inputData.find(delimiter);
+        pinName = inputData.substr(0, delimeterPosition);
+
+        // Skip unused rows
+        if (pinName != "" && pinName != "////MKM4//// ")
+            (*pins).push_back(pinName);
+    }
+
+    // Close input file
+    readData.close();
+}
+
+void collectStatistic(string directoryPath, vector <string> *pins, vector <int> *counterOpen, vector <int> *counterShort)
+{
+    /* Function for reading input data */
+
+    // Define variable that are help form full file name
     int counter = 1;
-    string filePath;
 
     // Bool variable to read undefined count of files
     bool fileExists = true;
 
-    // Base cycle
+    // Base cycle to read input file
     while (fileExists)
     {
         // Set full path to a file
-        filePath = directoryPath + to_string(counter) + inputFileExtension;
+        string filePath = directoryPath + to_string(counter) + inputFileExtension;
 
         // Create input stream
         ifstream readData(filePath);
@@ -37,52 +68,67 @@ void readInputData(string directoryPath, int *counterOpen, int *counterShort, in
         {
             // Variable to store input data
             string inputData;
+            string pinName;
 
-            // Read line
+            // Read a row from input
             while(getline(readData, inputData))
             {
-                size_t found;
+                // Mapping between contact name and its test case
+                // Defined as one to one relation
+                for (int i= 0; i < (*pins).size(); i++)
+                {
+                    delimeterPosition = inputData.find(delimiter);
 
-                found=inputData.find(openCase);
-                if (found!=string::npos)
-                    (*counterOpen)++;
+                    std::size_t foundopenCase   = inputData.find(openCase);
+                    std::size_t foundshortCase  = inputData.find(shortCase);
+                    pinName = inputData.substr(0, delimeterPosition);
+                    string temp = (*pins)[i];
+                    if (pinName == temp)
+                    {
+                        if (foundopenCase!=std::string::npos)
+                            ((*counterOpen)[i])++;
 
-                found=inputData.find(shortCase);
-                if (found!=string::npos)
-                    (*counterShort)++;
+                        if (foundshortCase!=std::string::npos)
+                            ((*counterShort)[i])++;
 
-                found=inputData.find(okCase);
-                if (found!=string::npos)
-                    (*counterOk)++;
+                    }
+                }
+
             }
-
-            readData.close();
-            counter++;
+          counter++;
+          readData.close();
         }
         else
-            fileExists = false;
+           fileExists = false;
     }
 }
 
-// Function to store statistic
-void writeStatistic(string directoryPath, int *counterOpen, int *counterShort, int *counterOk)
+void writeStatistic(string directoryPath, vector <string> *pins, vector <int> *counterOpen, vector <int> *counterShort)
 {
+    /* Function stores statistic */
+
     // Create output stream
     ofstream outfile;
+    string header;
+    string delimeter;
 
-    // Output file parameters
-    const string fileName = "_cases_statistic";
-    const string outputFileExtension = ".txt";
-    const string delimeter = "\t";
+    // Choose delimeter for output file
+    if (outputFileExtension == ".txt")
+        delimeter = "\t";
+    else if (outputFileExtension == ".csv")
+        delimeter = ",";
+
+    // Define header
+    header = "Pin name" + delimeter + shortCase + delimeter + openCase + "\n";
 
     // Forming output file name and creating it
     string fullOutFileName = directoryPath + fileName + outputFileExtension;
     outfile.open(fullOutFileName);
 
     // Write data
-    outfile << openCase << delimeter << *counterOpen << endl;
-    outfile << shortCase << delimeter << *counterShort << endl;
-    outfile << okCase << delimeter << *counterOk << endl;
+    outfile << header;
+    for (int i = 0; i < (*pins).size(); i++)
+        outfile << (*pins)[i] << delimeter << (*counterShort)[i] << delimeter << (*counterOpen)[i] << endl;
 
     outfile.close();
 }
@@ -90,17 +136,21 @@ void writeStatistic(string directoryPath, int *counterOpen, int *counterShort, i
 int main()
 
 {
-    // Define input parameters
+    // Define input path
     const string directoryPath = "D:\\projects\\res_OpenShortTest\\";
 
-    // Define variables to collect count of cases
-    int counterOpen = 0;
-    int counterShort = 0;
-    int counterOk = 0;
+    // Define vectors to collect count of cases
+    vector <string> pins;
+    collectPinNames(directoryPath, &pins);
 
-    readInputData(directoryPath, &counterOpen, &counterShort, &counterOk);
-    writeStatistic(directoryPath, &counterOpen, &counterShort, &counterOk);
+    int vectorSize = pins.size();
+    vector <int> counterOpen(vectorSize, 0);
+    vector <int> counterShort(vectorSize, 0);
+
+    collectStatistic(directoryPath, &pins, &counterOpen, &counterShort);
+    writeStatistic(directoryPath, &pins, &counterOpen, &counterShort);
 
     cout << "---------- Done ----------" << endl;
+
     return 0;
 }
